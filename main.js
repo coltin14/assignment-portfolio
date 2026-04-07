ae11294e15aff6106a72fd779ab38463eb7e6db3
coltin14 <coltpayton34@yahoo.com>
2026-04-07 09:45:48 -0500

    Added other assignments

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const assignments = [
     { name: "Assignment 2 — Resume", link: "https://jolly-stone-05ac6be10.4.azurestaticapps.net" },
     { name: "Assignment 3 — Dice Roller", link: "https://zealous-glacier-0ce124810.2.azurestaticapps.net" },
     { name: "Assignment 4 — Lion and Tigers", link: "https://jolly-field-0894e8710.2.azurestaticapps.net" },
-    { name: "Assignment 5 — Will update soon", link: "#" }
+    { name: "Assignment 5 — Web Dice Roller", link: "https://web-dice-roller-nodejs-fhggc7e2hyakdcep.centralus-01.azurewebsites.net" },
+    { name: "Assignment 5 — Client", link: "https://happy-tree-01d1df610.4.azurestaticapps.net" },
+    { name: "Assignment 6 — Health Insurance Risk Calculator", link: "https://agreeable-field-022eee810.1.azurestaticapps.net" }, 
+    { name: "Assignment 6 — Server", link: "https://health-insurance-risk-calc-server-backlog-fngae6hphbgcchd8.centralus-01.azurewebsites.net" },
 ];
 
 const list = document.getElementById("assignment-list");
